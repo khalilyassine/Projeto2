e3ecd718f57c6eda67c2e9f5db186bb4f8ef3244
MarceloHarad <marcelogdeandrade@gmail.com>
2016-03-31 08:26:16 -0300

    Mudanca nas classes especificas de porta logica (Not,Or,And) com a criacao dos Setters dos pinos A e B. Tambem foi adicionado o JUnit para teste das portas, iniciada a classe TestAndGate

---
+++ Projeto2/src/TestAndGate.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAndGate {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
--- Projeto2/src/modelo/AndGate.java
+++ Projeto2/src/modelo/AndGate.java
@@ -8,7 +8,15 @@ public class AndGate extends LogicGate{
     private InputPin pinA;
     private InputPin pinB;
 
-    @Override
+    public void setPinA(InputPin pinA) {
+		this.pinA = pinA;
+	}
+
+	public void setPinB(InputPin pinB) {
+		this.pinB = pinB;
+	}
+
+	@Override
     public boolean getOutputValue(int index) {
 
         boolean sinalA = pinA.getSource().getOutputValue(pinA.getIndex());
