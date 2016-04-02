27b179f2c64fa49ba4d24dc9855dbccb9d476f3a
rachelpbm <rachelpbm@gmail.com>
2016-04-02 17:02:10 -0300

    HalfAdder corrigida
    correção da HalfAdder!

--- Projeto2/src/modelo/HalfAdder.java
+++ Projeto2/src/modelo/HalfAdder.java
@@ -1,5 +1,33 @@
 package modelo;
 
-public class HalfAdder {
+public class HalfAdder extends LogicGate{
 
+    private InputPin pinA;
+    
+    public void setPinA(InputPin pinA) {
+		this.pinA = pinA;
+	}
+    
+	private InputPin pinB;
+
+	public void setPinB(InputPin pinB) {
+		this.pinB = pinB;
+	}
+
+
+    @Override
+    public boolean getOutputValue(int index) {
+
+        boolean sinalA = pinA.getSource().getOutputValue(pinA.getIndex());
+        boolean sinalB = pinB.getSource().getOutputValue(pinB.getIndex());
+        
+        if (index == 0){
+        	return ((!sinalA && sinalB) || (!sinalB && sinalA));
+        }
+        else{
+        	return (sinalA && sinalB);
+        }
+        
+    }
 }
+
