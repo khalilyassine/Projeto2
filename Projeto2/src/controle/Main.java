1ed59adee2c365e82a8214625779f778724baaed
MarceloHarad <marcelogdeandrade@gmail.com>
2016-04-04 00:33:32 -0300

    Adicionado pacote de controle (integracao de visao e modelo). Mudado local da main para controle e criacao de 2 novas classes (Main e ControleSaidas). Comeco da implementacao de controle de saida das portas(feito somente para porta AND e sem usar Lamp). Pequenas mudancas de teste na classe Lamp para utilizacao futura.

---
+++ Projeto2/src/controle/Main.java
@@ -0,0 +1,16 @@
+package controle;
+
+import modelo.AndGate;
+import modelo.InputPin;
+import modelo.LogicGate;
+import modelo.Switch;
+import visao.MainFrame;
+
+public class Main {
+    public static void main(String args[]){
+        MainFrame x = new MainFrame();
+        x.start();
+        
+
+    }
+}
